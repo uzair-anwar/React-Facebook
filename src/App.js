a6b261089e47181f263c3c543291788bad329488
uzair-anwar <uzair.anwar@devsinc.com>
2022-07-21 18:45:36 +0500

    Delete un neccesary files

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import Main from "./Components/Main";
-import Editpost from "./Components/editPost";
-import Showpost from "./Components/showPost";
-import EditComment from "./Components/editComment";
 
 function App() {
   const [user, setUser] = useState();
@@ -18,10 +15,6 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/main" element={<Main />} />
-          <Route path="/Post/:id" element={<Showpost />} />
-          <Route path="/Post/:id" element={<Showpost />} />
-          <Route path="/Post/:id/edit" element={<Editpost />} />
-          <Route path="/Post/:id/Comment/:id/edit" element={<EditComment />} />
         </Routes>
       </userContext.Provider>
     </BrowserRouter>
